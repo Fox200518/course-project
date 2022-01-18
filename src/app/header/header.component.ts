d594e6a53cae32ad08af3efe30c22e6ba41c36f0
Drew Snyder <Fox200518@gmail.com>
2022-01-17 20:32:21 -0500

    Setup Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
+
+  collapsed = true;
+
   constructor() {}
   ngOnInit() {}
 }
