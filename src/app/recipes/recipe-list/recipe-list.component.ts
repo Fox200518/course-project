6025985a144ae65753f55caa47ac9814e8cd0439
Drew Snyder <Fox200518@gmail.com>
2022-01-18 02:22:24 -0500

    Recipe List Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes : Recipe[] = [
+    new Recipe('dummy', 'dummy recipe', 'https://www.maxpixel.net/static/photo/1x/Borsch-A-Simple-Recipe-Cook-At-Home-Food-4260907.jpg')
+  ];
 
   constructor() { }
 
